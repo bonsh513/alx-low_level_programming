6263a2503ee046d53ca9b38c84390f25484833c8
bongole shafik <bonshreynah@gmail.com>
2022-06-30 16:18:49 -0700

    adding 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main is the fucntion that holds the code block
- * Return: (0)success
+ * main - holds code
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
