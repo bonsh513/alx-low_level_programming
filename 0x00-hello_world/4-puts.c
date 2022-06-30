78cd35f3b6051a2924d5821c9dbc1e4c3fdc0604
bongole shafik <bonshreynah@gmail.com>
2022-06-30 15:58:21 -0700

    adding 3-name&4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *
  * main is the fucntion that holds the code block
  *
+ * Return: (0)success
  */
 
 int main(void)
 {
-	puts	("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
