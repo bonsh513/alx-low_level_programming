53745404549355b29d336a72b374122627a58532
bongole shafik <bonshreynah@gmail.com>
2022-07-04 16:12:00 -0700

    Adding 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s %d is %d\n", last, n, n % 10);
+	printf("%s %d is %d", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("and is greater than 5");
+		printf("and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("and is 0");
+		printf("and is 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
