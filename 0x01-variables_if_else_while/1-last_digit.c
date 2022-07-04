7e3f9e8b0fc2aeb99e791f069a1c005890209228
bongole shafik <bonshreynah@gmail.com>
2022-07-04 16:06:09 -0700

    Adding 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - last random number is positive or negative
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char last[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%s %d is %d\n", last, n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf("and is greater than 5");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("and is 0");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0");
+	}
+	return (0);
+}
